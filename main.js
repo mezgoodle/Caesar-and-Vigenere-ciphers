d130c7088aed182fde9bfafcc788995e7aa395f4
mezgoodle <mezgoodle@gmail.com>
2020-03-26 13:26:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ const path = require('path');
 const url = require('url');
 const {app, BrowserWindow} = require('electron');
 
-let win;
-
 function createWindow() {
-    win = new BrowserWindow({
+    // Create the browser window.
+    let win = new BrowserWindow({
         width: 800,
         height: 600, 
         icon: __dirname + "/img/icon.png",
     });
 
+    // and load the index.html of the app.
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true,
     }));
 
+    // Open the DevTools.
     // win.webContents.openDevTools();
 
     win.on('closed', () => {
@@ -24,8 +25,22 @@ function createWindow() {
     });
 }
 
-app.on('ready', createWindow);
+// This method will be called when Electron has finished initialization and is ready to create browser windows. Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
 
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    app.quit();
-});
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
