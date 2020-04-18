5311dba9ea67eadd16990d023c230293c93d90d6
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-04-18 20:29:40 +0300

    Add data test

--- test.js
+++ test.js
@@ -1,4 +1,24 @@
-const fn = require("./main");
+const fn = require("./ciphers");
 const assert = require("assert").strict;
+
+const data = [{
+        value: "pellentesque",
+        amount: 12,
+        expected: "bqxxqzfqecgq",
+    },
+    {
+        value: "pellentesque",
+        amount: 0,
+        expected: "pellentesque",
+    },
+    {
+        value: "bqxxqzfqecgq",
+        amount: -12,
+        expected: "pellentesque",
+    },
+];
+for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+}
 const result = fn("Attack at dawn!", 12);
 assert.strictEqual(result, "Mffmow mf pmiz!", 'Negative string');
