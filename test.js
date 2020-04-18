0ca6d97096ca1ed2106a5a4c1c5af88ce7a02c0d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-04-18 21:43:29 +0300

    Update test.js

--- test.js
+++ test.js
@@ -16,6 +16,36 @@ const data = [{
         amount: -12,
         expected: "pellentesque",
     },
+    {
+        value: "adadA",
+        amount: 13123,
+        expected: "twtwT",
+    },
+    {
+        value: "ad_dA",
+        amount: 1,
+        expected: "be_eB",
+    },
+    {
+        value: "13123131",
+        amount: 5,
+        expected: "13123131",
+    },
+    {
+        value: "!@&*^",
+        amount: -134353,
+        expected: "!@&*^",
+    },
+    {
+        value: "!@$^@a",
+        amount: -1970,
+        expected: "!@$^@g",
+    },
+    {
+        value: "dada",
+        amount: 19.7,
+        expected: "wtwt",
+    },
 ];
 
 for (let i = 0; i < data.length; i++) {
