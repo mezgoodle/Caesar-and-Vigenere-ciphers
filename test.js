e1c830438e5ecb6778f3e755ccba596b5e4ae735
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-04-19 13:29:15 +0300

    Better output and delete cathc non-string types

--- test.js
+++ test.js
@@ -16,7 +16,7 @@ const tests = [
         ["adadA", 13123], "twtwT", "testUpperLowerCase"
     ],
     [
-        ["ad_dA", 1], "be_eB", "testStringWithSymbols"
+        ["ad_dA", 1], "be_eb", "testStringWithSymbols"
     ],
     [
         ["13123131", 5], "13123131", "testIntegers"
@@ -28,22 +28,20 @@ const tests = [
         ["!@$^@a", -1970], "!@$^@g", "testSymbolsWithString"
     ],
     [
-        ["dada", -19.70], "jgjg", "testFloatAmount"
-    ],
-    [
-        [123, 19.70], "!@&*^", "testNumberType"
-    ],
-    [
-        [undefined, -0], "!@&*^", "testUndefinedType"
+        ["dada", -19.70], "jgj", "testFloatAmount"
     ],
 ];
 
+// Output
+const results = [];
 for (const test of tests) {
     const [par, expected, name] = test;
     const result = fn(par[0], par[1]);
     try {
         assert.strictEqual(result, expected, `Error in test "${name}"`);
     } catch (err) {
-        console.log(err);
+        const { message, operator } = err;
+        results.push({ message, par, expected, result, operator });
     }
 }
+console.table(results);
