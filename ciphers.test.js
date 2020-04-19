08893225aa191ca5eebab32cf39314707f9bad42
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-04-19 17:06:37 +0300

    Add some tests for jest

--- ciphers.test.js
+++ ciphers.test.js
@@ -1,5 +1,15 @@
 const { caesarEncrypt } = require("./ciphers");
 
-test("testSimple", () => {
-    expect(caesarEncrypt("pellentesque", 12)).toBe("bqxxqzfqecgq");
+describe("caesarEncrypt:", () => {
+    test("testSimple", () => {
+        expect(caesarEncrypt("pellentesque", 12)).toBe("bqxxqzfqecgq");
+    });
+
+    test("testFloatAmount", () => {
+        expect(caesarEncrypt("dada", -19.70)).toBe("jgjg");
+    });
+
+    test("testZeroAmount", () => {
+        expect(caesarEncrypt("pellentesque", -0)).toBe("pellentesque");
+    });
 })
