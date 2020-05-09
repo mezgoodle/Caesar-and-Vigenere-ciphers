d98a764caa9c3b9a1b831e34888b78c25d4019a9
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-09 14:24:31 +0300

    Improve functions and delete duplicated code

--- index.js
+++ index.js
@@ -1,50 +1,28 @@
 const caesarEncrypt = (text = null, amount = null) => {
-
     if (text === null) throw Error("Message should be not empty");
-
     if (!Number.isInteger(amount) || amount === null) throw Error("Amount should be integer and not empty");
-
     // Wrap the amount
-    if (amount < 0) {
-        return caesarEncrypt(text, amount + 26);
-    }
-
-    if (typeof(text) !== "string") {
-        throw Error("String or number expected");
-    }
+    if (amount < 0) return caesarEncrypt(text, amount + 26);
+    if (typeof(text) !== "string") throw Error("String or number expected");
 
     // Make an output letiable
     let output = "";
-
     // Go through each character
     for (let i = 0; i < text.length; i++) {
-
         // Get the character we'll be appending
         let c = text[parseInt(i)];
-
         // If it's a letter...
         if (isLetter(c)) {
-
             // Get its code
             let code = text.charCodeAt(i);
-
             // Uppercase letters
-            if ((code >= 65) && (code <= 90)) {
-                c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-            }
-
+            if ((code >= 65) && (code <= 90)) c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
             // Lowercase letters
-            else if ((code >= 97) && (code <= 122)) {
-                c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
-            }
-
-        }
-
+            else if ((code >= 97) && (code <= 122)) c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+        };
         // Append
         output += c;
-
-    }
-
+    };
     // All done!
     return output;
 };
@@ -62,59 +40,40 @@ const isLetter = (c) => (c.match(/[a-zA-Z]+/));
 
 const isUpperCase = (c) => (c.match(/[A-Z]+/));
 
-const vigenereEncryptChar = (t, k) => {
-    if (!isLetter(t) || !isLetter(k)) return t;
-    let uppercase = isUpperCase(t);
-    t = t.toLowerCase().charCodeAt(0);
+const workerChar = (char, k, type = "e") => {
+    if (!isLetter(char) || !isLetter(k)) return char;
+    let uppercase = isUpperCase(char);
+    char = char.toLowerCase().charCodeAt(0);
     k = k.toLowerCase().charCodeAt(0);
     a = "a".charCodeAt(0);
     A = "A".charCodeAt(0);
-    return String.fromCharCode((uppercase ? A : a) + (((t - a) + (k - a)) % 26));
+    if (type === "d") {
+        let t = a + ((char - a) - (k - a));
+        if (t < a) t += 26;
+        t = String.fromCharCode(t);
+        return uppercase ? t.toUpperCase() : t;
+    } else return String.fromCharCode((uppercase ? A : a) + (((char - a) + (k - a)) % 26));
 };
 
-const vigenereDecryptChar = (c, k) => {
-    if (!isLetter(c) || !isLetter(k)) return c;
-    let uppercase = isUpperCase(c);
-    c = c.toLowerCase().charCodeAt(0);
-    k = k.toLowerCase().charCodeAt(0);
-    a = "a".charCodeAt(0);
-    A = "A".charCodeAt(0);
-    let t = a + ((c - a) - (k - a));
-    if (t < a) t += 26;
-    t = String.fromCharCode(t);
-    return uppercase ? t.toUpperCase() : t;
-};
-
-const vigenereEncryptText = (text = null, key = null) => {
+const worker = (str, key, type) => {
     key = keepLetters(key);
-    let cipher = "",
+    let result = "",
         keyIndex = 0;
-    for (let i = 0; i < text.length; i++) {
-        let t = text.charAt(i);
-        if (isLetter(t)) {
+    for (let i = 0; i < str.length; i++) {
+        let char = str.charAt(i);
+        if (isLetter(char)) {
             k = key.charAt(keyIndex++ % key.length);
-            c = vigenereEncryptChar(t, k);
-            cipher += c;
-        } else cipher += t;
-    }
-    return cipher;
-};
-
-const vigenereDecryptText = (cipher = null, key = null) => {
-    key = keepLetters(key);
-    let text = "",
-        keyIndex = 0;
-    for (let i = 0; i < cipher.length; ++i) {
-        let c = cipher.charAt(i);
-        if (isLetter(c)) {
-            k = key.charAt(keyIndex++ % key.length);
-            t = vigenereDecryptChar(c, k);
-            text += t;
+            temp = workerChar(char, k, type);
+            result += temp;
         } else {
-            text += c;
+            result += char;
         }
     }
-    return text;
+    return result;
 };
+
+const vigenereEncryptText = (text = null, key = null) => (worker(text, key, "e"));
+
+const vigenereDecryptText = (cipher = null, key = null) => (worker(cipher, key, "d"));
 
 module.exports = { caesarEncrypt, caesarDecrypt, vigenereEncryptText, vigenereDecryptText };
